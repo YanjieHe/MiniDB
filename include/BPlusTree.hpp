3e8d9351c0fdad42d809a4b07d751496c2fa53dd
Yanjie He <heyanjie0@outlook.com>
2021-12-03 16:57:26 -0500

    conversion between index and data base row

--- include/BPlusTree.hpp
+++ include/BPlusTree.hpp
@@ -1,8 +1,25 @@
 #ifndef B_PLUS_TREE_HPP
 #define B_PLUS_TREE_HPP
+#include "BufferManager.hpp"
+#include "DBIndex.hpp"
+#include "DBRow.hpp"
 
 class BPlusTree {
 public:
+  BufferManager &bufferManager;
+  u16 rootPageID;
+  size_t pageSize;
+  size_t maxNumOfKeysInOnePage;
+  Buffer buffer;
+  vector<DBColumn> columns;
+
+  BPlusTree(BufferManager &bufferManager, u16 rootPageID, size_t pageSize, size_t maxNumOfKeysInOnePage,
+            const vector<DBColumn> &columns);
+
+  void Insert(DBIndex indexToInsert, DBRow data);
+
+  static DBIndex GetIndexFromRow(const DBRow &row);
+  static DBRow ConvertIndexToRow(const DBIndex& index, i64 dataPointerVal);
 };
 
 #endif // B_PLUS_TREE_HPP
--- src/BPlusTree.cpp
+++ src/BPlusTree.cpp
@@ -1,1 +1,61 @@
 #include "BPlusTree.hpp"
+#include "Page.hpp"
+#include "DBException.hpp"
+
+using std::holds_alternative;
+
+BPlusTree::BPlusTree(BufferManager &bufferManager, u16 rootPageID,
+                     size_t pageSize, size_t maxNumOfKeysInOnePage,
+                     const vector<DBColumn> &columns)
+    : bufferManager{bufferManager}, rootPageID{rootPageID}, pageSize{pageSize},
+      maxNumOfKeysInOnePage{maxNumOfKeysInOnePage}, buffer(pageSize),
+      columns{columns} {}
+
+void BPlusTree::Insert(DBIndex indexToInsert, DBRow data) {
+  bufferManager.LoadBuffer(rootPageID, buffer);
+  Page page(columns, buffer, pageSize);
+
+  for (size_t i = 0; i < page.NumOfRows(); i++) {
+    DBRow row = page.GetRow(buffer, i);
+    i64 dataPointerVal = std::get<i64>(row.values[0]);
+    DBIndex index = GetIndexFromRow(row);
+    if (CompareIndex(indexToInsert, index) < 0) {
+      // found the position for the new index
+      if (page.NumOfRows() >= maxNumOfKeysInOnePage) {
+
+      } else {
+        // insert the new key-value pair in the current page
+      }
+    }
+  }
+}
+
+DBIndex BPlusTree::GetIndexFromRow(const DBRow &row) {
+  if (row.values.size() <= 1) {
+    throw DBException("wrong number of items in a row for index");
+  }
+  vector<DBIndex::Key> keys(row.values.size() - 1);
+  for (size_t i = 1; i < row.values.size(); i++) {
+    if (holds_alternative<i64>(row.values[i])) {
+      keys[i - 1] = std::get<i64>(row.values[i]);
+    } else if (holds_alternative<string>(row.values[i])) {
+      keys[i - 1] = std::get<string>(row.values[i]);
+    } else {
+      throw DBException("wrong type for index");
+    }
+  }
+  return DBIndex(keys);
+}
+
+static DBRow ConvertIndexToRow(const DBIndex &index, i64 dataPointerVal) {
+  vector<DBRow::Value> values(index.keys.size() + 1);
+  values[0] = dataPointerVal;
+  for (size_t i = 0; i < index.keys.size(); i++) {
+    if (holds_alternative<i64>(index.keys[i])) {
+      values[i + 1] = std::get<i64>(index.keys[i]);
+    } else {
+      values[i + 1] = std::get<string>(index.keys[i]);
+    }
+  }
+  return DBRow(values);
+}
