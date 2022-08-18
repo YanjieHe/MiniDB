8faa78a625ec67f0578b6af54dee698ae7e422ca
Yanjie He <heyanjie0@outlook.com>
2022-08-17 20:35:24 -0400

    initialize the nPages field in the DatabaseHeader

--- test/TestBPlusTree.cpp
+++ test/TestBPlusTree.cpp
@@ -12,10 +12,11 @@ using std::endl;
 
 const size_t order = 3;
 
-TEST_CASE("Test B+ Tree", "[B+ Tree]") {
+TEST_CASE("Book Example B+ Tree Test", "[B+ Tree]") {
   auto columns = BookIndexColumn();
   auto buffer = CreateBookPage();
   DatabaseHeader dbHeader;
+  dbHeader.nPages = 0;
   dbHeader.pageSize = PAGE_SIZE;
 
   SECTION("single index: book example") {
@@ -29,17 +30,17 @@ TEST_CASE("Test B+ Tree", "[B+ Tree]") {
     tree.Insert(DBIndex({StringKey("The Catcher in the Rye")}), 30);
     tree.Insert(DBIndex({StringKey("Pride and Prejudice")}), 40);
 
-    for (int pageNumber = 0; pageNumber < 3; pageNumber++) {
-      bufferManager.LoadBuffer(pageNumber, tree.sharedData.buffer);
-      IndexPage indexPage(tree.sharedData.columns, tree.sharedData.buffer,
-                          PAGE_SIZE);
+    // for (int pageNumber = 0; pageNumber < 3; pageNumber++) {
+    //   bufferManager.LoadBuffer(pageNumber, tree.sharedData.buffer);
+    //   IndexPage indexPage(tree.sharedData.columns, tree.sharedData.buffer,
+    //                       PAGE_SIZE);
 
-      cout << "page " << pageNumber << endl;
-      cout << std::setw(4)
-           << JsonSerializer::BPlusTreePageToJson(indexPage,
-                                                  tree.sharedData.buffer)
-           << endl;
-    }
+    //   cout << "page " << pageNumber << endl;
+    //   cout << std::setw(4)
+    //        << JsonSerializer::BPlusTreePageToJson(indexPage,
+    //                                               tree.sharedData.buffer)
+    //        << endl;
+    // }
 
     REQUIRE(tree.Search(DBIndex({StringKey("The Red and the Black")})) ==
             std::make_optional<i64>(10));
@@ -49,5 +50,36 @@ TEST_CASE("Test B+ Tree", "[B+ Tree]") {
             std::make_optional<i64>(30));
     REQUIRE(tree.Search(DBIndex({StringKey("Pride and Prejudice")})) ==
             std::make_optional<i64>(40));
+  }
+}
+
+TEST_CASE("Integer Index B+ Tree Test", "[B+ Tree]") {
+  vector<DBColumn> indexColumns{DBColumn(false, TypeTag::INTEGER, "index")};
+  vector<DBColumn> dataColumns{DBColumn(false, TypeTag::INTEGER, "index"),
+                               DBColumn(false, TypeTag::INTEGER, "value")};
+  DatabaseHeader dbHeader;
+  dbHeader.nPages = 0;
+  dbHeader.pageSize = PAGE_SIZE;
+
+  SECTION("single index: integer index example") {
+    string path = "output/integer_b_plus_tree";
+    CreateEmptyDatabaseFile(path, dbHeader);
+    BufferManager bufferManager(path, dbHeader);
+
+    BPlusTree tree(order, bufferManager, PAGE_SIZE, indexColumns);
+    for (int i = 0; i < 3; i++) {
+      tree.Insert(DBIndex({56 + i}), (i + 1) * 10);
+    }
+    // for (int pageNumber = 0; pageNumber < 3; pageNumber++) {
+    //   bufferManager.LoadBuffer(pageNumber, tree.sharedData.buffer);
+    //   IndexPage indexPage(tree.sharedData.columns, tree.sharedData.buffer,
+    //                       PAGE_SIZE);
+
+    //   cout << "page " << pageNumber << endl;
+    //   cout << std::setw(4)
+    //        << JsonSerializer::BPlusTreePageToJson(indexPage,
+    //                                               tree.sharedData.buffer)
+    //        << endl;
+    // }
   }
 }
